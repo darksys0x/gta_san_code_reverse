8389abd8b8c2dc8c5bf4d39e492a4d29c2d3f150
darksys0x <darksys0x@gamail.com>
2024-10-23 02:03:20 +0300

    partial reversed code for sub_4E2350

---
+++ RE_Assembly_GTA.cpp
@@ -0,0 +1,78 @@
+// RE_Assembly_GTA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class MYClass1 {
+public:
+	char sub_4D88C0(int a, int b) 
+	{ // __thiscalall
+
+	}
+	void sub_4D88A0(int a, int b);
+
+};
+
+ 
+MYClass1 g_Object;
+
+
+char dword_BD00F8[]; 
+int dword_B7CB84; //<- mov edi, dword_B7CB84
+
+class Myclass2 {	
+public:
+	char sub_506D90();
+	
+
+};
+
+Myclass2 g_obj1;
+
+void sub_4E2350(int arg1) {
+	int var1;
+	int var2;
+	int var2;
+	
+	int something = dword_BD00F8[arg1]; //ecx, byte ptr [eax+ebp] |  mov [esp+1Ch+arg_0], ecx
+	
+
+
+	int a = 3;
+	int c = a;
+
+
+
+
+	char result = g_Object.sub_4D88C0(0x8F, 5);
+	if (result == 0) {
+
+		char hamadResult = g_obj1.sub_506D90();
+		if (hamadResult) {
+			return;
+		}
+		g_Object.sub_4D88A0(0x8f, 5);
+
+
+		return;
+	}
+
+	else {
+		
+	}
+
+
+	
+}
+
+
+
+int main()
+{
+  
+
+
+
+
+}
+
